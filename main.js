c8ad21397554858c8bab9bd4ba45d4aa1d864aa4
ghiottoandrea <andreaghiotto9@gmail.com>
2024-02-19 16:38:44 +0100

    add js code

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 //Chiedo di inserire età e km con un promt
+
+const distance = prompt("Inserisci quanti KM intendi percorrere?")
+const age = prompt("Inserisci la tua età?")
+
 //Calcola il costo, in base alla età applico lo sconto
 //Arrotondo e lo stampo in console
+let price = (distance * 0.21)
 
-
+if (age < 18){
+    let discount = (price * 20 / 100);
+    let price_discounted = (price - discount);
+    let price_arounded = price_discounted.toFixed(2);
+    alert("In totale sono "+price_arounded+"€")
+}else if (age > 65) {
+    let discount = (price * 40 / 100);
+    let price_discounted = (price - discount);
+    let price_arounded = price_discounted.toFixed(2);
+    alert("In totale sono "+price_arounded+"€")
+}else{
+    let price_arounded = price.toFixed(2);
+    alert("In totale sono "+price_arounded+"€")
+}
 
--- readme.txt
+++ readme.txt
@@ -8,8 +8,15 @@ Un biglietto del treno da calcolare a 0.21€/km in più si applica uno sconto d
 -allert
 
 
-/*
+-Esempio to fixed
 let numero = 10.56789;
 let numeroArrotondato = numero.toFixed(2);
 console.log(numeroArrotondato); // Output: 10.57
+
+
+/*
+Esempio: per 100km sono 21€
+per i minorenni sono 4.2€ di sconto cioè 16.80€
+per anziani sono 8.4€ di sonto cioè 12.60€
+per gli altri sono 21€
 */
